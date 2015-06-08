0dec506d27f96ac5d808c3c09ecf29a1dbe6e4cc
Harry Xue <wxue@college.harvard.edu>
2015-06-08 15:34:16 -0400

    added assignments, still need to format time

--- mysite/syllabuspdf/htmltopdf.py
+++ mysite/syllabuspdf/htmltopdf.py
@@ -1,5 +1,7 @@
 # Given a course id and oauth token (hardcoded right now), exports syllabus as PDF
 import requests
+import datetime
+from re import sub
 
 from xhtml2pdf import pisa
 from StringIO import StringIO
@@ -13,27 +15,61 @@ courseid = 2502
 # Base URL
 baseurl = "https://canvas.harvard.edu/api/v1/courses/"
 
-def create_URL ():
+def create_URL (id):
 	'''Create URL for API request'''
-	return baseurl + str(courseid)
+	return baseurl + str(id)
 
 def fetch_syllabus(id, token):
 	'''Fetches syllabus for course with given id and token from API. Returns syllabus as HTML string'''
 	
 	headers = {"Authorization": "Bearer " + token}
 	params = {'include[]': 'syllabus_body'}
-	requesturl = create_URL()
+	requesturl = create_URL(id)
 
 	# Make request
 	r = requests.get(requesturl, headers = headers, params = params)
 
-	# Parse JSON
-	parsedJSON = r.json()
+	# Decode JSON
+	syllabus = r.json()
 
-	return parsedJSON[u'syllabus_body']
+	return syllabus['syllabus_body']
+
+def fetch_assignments(id, token):
+	'''Fetches assignments for course with given id and token'''
+
+	headers = {"Authorization": "Bearer " + token} 
+	requesturl = create_URL(id) + "/assignments"
+
+	# Make request
+	r = requests.get(requesturl, headers = headers)
+	assignments = r.json()
+
+	# Sort assignments by time
+	sortedassign = sorted(assignments, key = lambda a: a['due_at'])
+	
+	# Format times of all assigments
+	for assignment in sortedassign:
+		if isinstance(assignment['due_at'], str):
+			assignment['due_at'] = format_time(assignment['due_at'])
+
+	return sortedassign
+
+def format_time(str):
+	'''Converts a time string into a more readable format'''
+	
+	# Clean string by stripping all non numeric characters
+	cleaned = sub("[^0-9]", "", str)
+
+	# Store formatted time
+	tformatted = datetime.datetime.strptime(cleaned, "%Y%m%d%H%M%S")
+
+	# Time string in format for display on webpage
+	time = tformatted.strftime('%a %b %d, %Y')
+
+	return time
 
 def HTML_to_PDF(html):
-	'''Converts HTML string to PDF'''
+	'''Converts HTML string to PDF string'''
 	pdf = StringIO()
 	pisaStatus = pisa.CreatePDF(StringIO(html.encode('utf-8')), pdf)
 	return pdf.getvalue()
