8b5471cbb8d7b43d273f75ebc199fdae60d66283
Harry Xue <wxue@college.harvard.edu>
2015-06-10 13:14:56 -0400

    added time generation

--- mysite/syllabuspdf/htmltopdf.py
+++ mysite/syllabuspdf/htmltopdf.py
@@ -1,25 +1,15 @@
 # Helper functions for generation of pdf view
 import requests
 
-# Oauth token for testing
-oauthtoken = "1875~6EHVRVunNzwmeaHXOm4Yji3uZOh3baRLVJU4yT6UO4NLnCRUYA0ByAx1pQi1IbGy"
-
-# Sample Course ID
-courseid = 2502
-
 # Base URL
-baseurl = "https://canvas.harvard.edu/api/v1/courses/"
-
-def create_URL (id):
-	'''Create URL for API request'''
-	return baseurl + str(id)
+baseurl = "https://canvas.harvard.edu/api/v1/"
 
 def fetch_syllabus(id, token):
 	'''Fetches syllabus for course with given id and token from API. Returns syllabus as HTML string'''
 	
 	headers = {"Authorization": "Bearer " + token}
 	params = {'include[]': 'syllabus_body'}
-	requesturl = create_URL(id)
+	requesturl = baseurl + "courses/" + str(id) 
 
 	# Make request
 	r = requests.get(requesturl, headers = headers, params = params)
@@ -29,22 +19,26 @@ def fetch_syllabus(id, token):
 
 	return syllabus['syllabus_body']
 
-def fetch_assignments(id, token):
-	'''Fetches assignments for course with given id and token'''
+def fetch_allevents(id, token):
+	'''Fetches assignments and events for course with given id and token'''
 
-	headers = {"Authorization": "Bearer " + token} 
-	requesturl = create_URL(id) + "/assignments"
+	headers = {"Authorization": "Bearer " + token}
+	assignparams = {"all_events": "true", "type" : "assignment", "context_codes[]" : "course_" + str(id)}
+	eventparams = {"all_events": "true", "context_codes[]" : "course_" + str(id)} 
 
-	# Make request
-	r = requests.get(requesturl, headers = headers)
-	assignments = r.json()
+	requesturl = baseurl + "calendar_events/"
+
+	# Make requests
+	assignr = requests.get(requesturl, headers = headers, params = assignparams)
+	eventr = requests.get(requesturl, headers = headers, params = eventparams)
+	
+	assignments = assignr.json()
+	events = eventr.json()
+
+	# Merge lists
+	allevents = assignments + events
 
 	# Sort assignments by time
-	sortedassign = sorted(assignments, key = lambda a: a['due_at'])
-	
-	# Format times of all assigments
-	for assignment in sortedassign:
-		if isinstance(assignment['due_at'], str):
-			assignment['due_at'] = format_time(assignment['due_at'])
+	sortedevents = sorted(allevents, key = lambda a: a['end_at'])
 
-	return sortedassign
+	return sortedevents
--- mysite/syllabuspdf/templatetags/syllabuspdf_extras.py
+++ mysite/syllabuspdf/templatetags/syllabuspdf_extras.py
@@ -1,21 +1,65 @@
-import datetime
+from datetime import datetime
+from dateutil import tz
 from django import template
 from django.template.defaultfilters import stringfilter
 from re import sub
 
 register = template.Library()
 
+def convert_tz(datetimeobj):
+	'''Converts a datetimeobj from UTC to the local timezone'''
+	from_zone = tz.tzutc()
+	to_zone = tz.gettz('America/New_York')
+	# Tell datetime object it's in UTC
+	utc = datetimeobj.replace(tzinfo=from_zone)
+	# Convert to local time
+	local = utc.astimezone(to_zone)
+
+	return local
+
 @register.filter
-def format_time(str):
-	'''Converts a time string into a more readable format'''
-	
+def format_date(str):
+	'''Converts a date string into a more readable format'''	
+	# Check for None case
+	if str is None:
+		return ""
+
 	# Clean string by stripping all non numeric characters
 	cleaned = sub("[^0-9]", "", str)
 
-	# Store formatted time
-	tformatted = datetime.datetime.strptime(cleaned, "%Y%m%d%H%M%S")
+	# Store formatted date as datetimeobject and convert timezone
+	dformatted = convert_tz(datetime.strptime(cleaned, "%Y%m%d%H%M%S"))
 
-	# Time string in format for display on webpage
-	time = tformatted.strftime('%a %b %d, %Y')
+	# Date string in format for display on webpage
+	date = dformatted.strftime('%a %b %d, %Y')
 
-	return time
+	return date
+
+@register.simple_tag
+def format_time(startat, endat):
+	'''Formats time for events, handling assignment events and calendar events separately'''
+	# Check for None
+	if (startat is None) or (endat is None):
+		return ""
+
+	# Clean strings, cutting out last six characters
+	cleanedstart = sub("[^0-9]", "", startat[0:20])
+	cleanedend = sub("[^0-9]", "", endat[0:20])
+
+	# If start_at and end_at are the same, the event is an assignment
+	if startat == endat:
+		tformatted = convert_tz(datetime.strptime(cleanedstart, "%Y%m%d%H%M%S"))
+		time = tformatted.strftime("%I:%M%p")
+		return "due by " + time
+
+	# Else calendar event
+	else:
+		sformatted = convert_tz(datetime.strptime(cleanedstart, "%Y%m%d%H%M%S"))
+		eformatted = convert_tz(datetime.strptime(cleanedend, "%Y%m%d%H%M%S"))
+		start = sformatted.strftime("%I:%M%p")
+		end = eformatted.strftime("%I:%M%p")
+		return start + " to " + end
+
+
+
+
