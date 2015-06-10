185db6ca3a54cfb9d056bfc400a09838e09f75cb
Harry Xue <wxue@college.harvard.edu>
2015-06-10 17:00:29 -0400

    added support for paginated results

--- mysite/syllabuspdf/apirequest.py
+++ mysite/syllabuspdf/apirequest.py
@@ -1,9 +1,7 @@
 # Makes Canvas API requests using the Canvas Python SDK
 from canvas_sdk.client import auth, base, request_context
-
 from canvas_sdk.utils import get_all_list_data
 
-courseid = 775
 oauthtoken = "1875~6EHVRVunNzwmeaHXOm4Yji3uZOh3baRLVJU4yT6UO4NLnCRUYA0ByAx1pQi1IbGy"
 
 baseurl = "https://canvas.harvard.edu/api"
@@ -28,16 +26,13 @@ def fetch_allevents(id):
 	# Create request context
 	req_context = request_context.RequestContext(oauthtoken, baseurl)
 
+	# Create separate parameters for assignment event and calendar event requests
 	assignparams = {"all_events": "true", "type" : "assignment", "context_codes[]" : "course_" + str(id)}
 	eventparams = {"all_events": "true", "context_codes[]" : "course_" + str(id)} 
 
 	# Make requests
-	assignr = base.call("GET", baseurl + "/v1/calendar_events" , req_context, params=assignparams, auth_token=oauthtoken)
-	eventr = base.call("GET", baseurl + "/v1/calendar_events" , req_context, params=eventparams, auth_token=oauthtoken)
-	
-	# Decode JSON
-	assignments = assignr.json()
-	events = eventr.json()
+	assignments = get_all_list_data(req_context, base.get, baseurl + "/v1/calendar_events", params=assignparams, auth_token=oauthtoken)
+	events = get_all_list_data(req_context, base.get, baseurl + "/v1/calendar_events", params=eventparams, auth_token=oauthtoken)
 
 	# Merge lists
 	allevents = assignments + events
--- mysite/syllabuspdf/views.py
+++ mysite/syllabuspdf/views.py
@@ -5,8 +5,8 @@ from apirequest import fetch_syllabus, fetch_allevents
 from easy_pdf.views import PDFTemplateView
 
 def index(request):
-	syllabus = fetch_syllabus(775)
-	events = fetch_allevents(775)
+	syllabus = fetch_syllabus(2502)
+	events = fetch_allevents(2502)
 	context = {'syllabus': syllabus, 'events': events}
 	return render(request,'syllabuspdf/index.html', context)
 
@@ -17,8 +17,8 @@ class SyllabusPDFView(PDFTemplateView):
         return super(SyllabusPDFView, self).get_context_data(
             pagesize="A4",
             title="Syllabus",
-            syllabus = fetch_syllabus(775),
-			events = fetch_allevents(775),
+            syllabus = fetch_syllabus(2502),
+			events = fetch_allevents(2502),
             **kwargs
         )
 
