03a696197fd7661b84c1223b346303f0748e7e33
Harry Xue <wxue@college.harvard.edu>
2015-06-18 12:43:56 -0400

    Almost done with testing

--- mysite/syllabuspdf/tests/test_syllabuspdf_extras.py
+++ mysite/syllabuspdf/tests/test_syllabuspdf_extras.py
@@ -4,13 +4,21 @@ from syllabuspdf.templatetags.syllabuspdf_extras import convert_tz, format_date,
 
 class SyllabusPDF_ExtrasTestCase(TestCase):
 	def test_convert_tz(self):
-		pass
+		from datetime import datetime
+		from django.utils import timezone
+		from dateutil import tz
+		self.assertEqual(datetime(2015,3,10,4,57,44).replace(tzinfo=tz.gettz('America/New_York')), convert_tz(datetime(2015,3,10,8,57,44)))
 
 	def test_format_date(self):
 		self.assertEqual(format_date(None), "")
+		self.assertEqual(format_date("2015-03-10T22:57:44Z"), "Tue Mar 10, 2015")
 
 	def test_format_time(self):
-		pass
+		self.assertEqual(format_time(None, ""), "")
+		self.assertEqual(format_time("", None), "")
+		self.assertEqual(format_time(None, None), "")
+		self.assertEqual(format_time("2015-03-10T22:57:44Z", "2015-03-10T22:57:44Z"), "due by 06:57PM")
+		self.assertEqual(format_time("2015-03-10T21:57:44Z", "2015-03-10T22:57:44Z"), "05:57PM to 06:57PM")
 
 	def test_escape_None(self):
 		self.assertEqual(escape_None(None), "")
