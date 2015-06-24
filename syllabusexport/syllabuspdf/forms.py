1507b1a75f9c5ae05ffb1e80c15a9cec1b86da09
Harry Xue <wxue@college.harvard.edu>
2015-06-24 13:35:13 -0400

    Manually generated form working

--- syllabusexport/syllabuspdf/forms.py
+++ syllabusexport/syllabuspdf/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 
 class SettingsForm(forms.Form):
-	syllabus = forms.BooleanField(label="Syllabus", initial=True, required=False)
-	dated_events = forms.BooleanField(label="Dated Events", initial=True, required=False)
-	undated_events = forms.BooleanField(label="Undated Events", initial=False, required=False)
-	descriptions = forms.BooleanField(label="Event Descriptions", initial=False, required=False)
-	times = forms.BooleanField(label="Event Times", initial=True, required=False)
-	weights = forms.BooleanField(label="Assignment Weights", initial=True, required=False)
-	hidden_field = forms.CharField(widget=forms.HiddenInput(), initial='field', required=False)
+	syllabus = forms.BooleanField(label="Syllabus Description", initial=True, required=False, label_suffix='')
+	dated_events = forms.BooleanField(label="Dated", initial=True, required=False, label_suffix='')
+	undated_events = forms.BooleanField(label="Undated", initial=False, required=False, label_suffix='')
+	descriptions = forms.BooleanField(label="Descriptions", initial=False, required=False, label_suffix='')
+	times = forms.BooleanField(label="Times", initial=True, required=False, label_suffix='')
+	weights = forms.BooleanField(label="Assignment Group Weights", initial=True, required=False, label_suffix='')
+	hidden_field = forms.CharField(widget=forms.HiddenInput(), initial="viewed", required=False, label_suffix='')
 
 	
