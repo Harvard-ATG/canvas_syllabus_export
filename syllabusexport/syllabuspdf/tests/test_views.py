26bf799570679875c4409b1f64754cd8e6929cba
Harry Xue <wxue@college.harvard.edu>
2015-06-23 20:24:38 -0400

    removed test_views

--- syllabusexport/syllabuspdf/tests/test_views.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from django.test import Client
-
-class SyllabusPDFTestCase(TestCase):
-	def test_index(self):
-		c = Client()
-		r = c.get('/syllabuspdf/')
-		self.assertEqual(r.status_code, 200) 
-
