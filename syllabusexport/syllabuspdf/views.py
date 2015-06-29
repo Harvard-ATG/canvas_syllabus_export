61b695cc91fda475e298d8764082871ee5beb7ca
Harry Xue <wxue@college.harvard.edu>
2015-06-29 10:20:15 -0400

    Added some comments

--- syllabusexport/syllabuspdf/views.py
+++ syllabusexport/syllabuspdf/views.py
@@ -11,18 +11,29 @@ def index(request):
 	except:
 		return HttpResponse("Course ID not found")
 
+	# Get content via API calls
 	syllabus = fetch_syllabus(courseid)
 	(dated, undated) = fetch_allevents(courseid)
 	groups = fetch_assigngroups(courseid)
 
+	# Check for initial load of page through presence of hidden field
 	if 'hidden_field' in request.GET:
 		form = SettingsForm(request.GET)
 		if form.is_valid():
 			settings = form.cleaned_data
 	else:
 		form = SettingsForm()
-		settings = {'syllabus':True,'dated_events':True,'undated_events':False,'descriptions':False,'times':True,'weights':False,'hidden_field':"viewed"}
+		settings = {
+			'syllabus':True,
+			'dated_events':True,
+			'undated_events':False,
+			'descriptions':False,
+			'times':True,
+			'weights':False,
+			'hidden_field':"viewed"
+		}
 
+	# Populate events array based on toggled options
 	events = []
 	if settings['dated_events']:
 		events += dated
