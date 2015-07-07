70991c8d5720b80f7868eca7c993c19a0d71b0d4
Harry Xue <wxue@college.harvard.edu>
2015-07-07 11:08:30 -0400

    Modified static paths

--- canvas_syllabus_export/settings/aws.py
+++ canvas_syllabus_export/settings/aws.py
@@ -132,8 +132,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT = '/home/ubuntu/static'
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'http_static'))
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ('/home/ubuntu/canvas-syllabus-export/canvas_syllabus_export/syllabuspdf/static', )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'syllabusexport', 'syllabuspdf', 'static'),
+)
 
 
