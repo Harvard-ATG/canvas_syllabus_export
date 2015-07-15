86e3ca0eed614e57b041e76c4eb3f9a500ca4bfc
Harry Xue <wxue@college.harvard.edu>
2015-07-15 12:13:37 -0400

    Added postgres as database

--- canvas_syllabus_export/settings/aws.py
+++ canvas_syllabus_export/settings/aws.py
@@ -115,9 +115,13 @@ WSGI_APPLICATION = 'canvas_syllabus_export.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': SECURE_SETTINGS.get('db_default_name', 'canvas_syllabus_export'),
+        'USER': SECURE_SETTINGS.get('db_default_user', 'postgres'),
+        'PASSWORD': SECURE_SETTINGS.get('db_default_password'),
+        'HOST': SECURE_SETTINGS.get('db_default_host', '127.0.0.1'),
+        'PORT': SECURE_SETTINGS.get('db_default_port', 5432),
+    } 
 }
 
 
