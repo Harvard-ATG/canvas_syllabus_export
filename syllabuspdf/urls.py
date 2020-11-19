f7687fca94d93372aac518abd267d2e69c793e9e
Joshua Getega <jgetega@gmail.com>
2020-11-19 23:37:30 +0300

    Refactor code for generating the LTI XML config
    Use our fork of the django-lti-provider package to generate the XML configuration instead of custom code within the canvas_syllabus_export code.

--- syllabuspdf/urls.py
+++ syllabuspdf/urls.py
@@ -5,6 +5,6 @@ from lti_provider import views as lti_views
 app_name = 'syllabuspdf'
 urlpatterns = [
     path('lti/launch/', views.process_lti_launch_request_view, name='process_lti_launch_request'),
-    path('lti/config/', views.LTIToolConfigView.as_view(), name='get_lti_xml'),
+    path('lti/config/', lti_views.LTIConfigView.as_view(), name='get_lti_xml'),
 	path('index', views.index, name='index'),
 ]
--- syllabuspdf/views.py
+++ syllabuspdf/views.py
@@ -6,7 +6,6 @@ from django.views.generic import View
 from django.conf import settings
 from lti_provider.lti import LTI
 from pylti.common import LTIException
-from lti import ToolConfig
 from urllib.parse import parse_qs, urlparse, urlencode, urlunparse
 from .apirequest import fetch_syllabus, fetch_allevents, fetch_assigngroups
 from .forms import SettingsForm
@@ -15,7 +14,6 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-LTI_SETUP = settings.LTI_SETUP
 
 @csrf_exempt
 def process_lti_launch_request_view(request):
@@ -88,65 +86,3 @@ def index(request):
         'settings': form_settings
     }
     return render(request, 'syllabuspdf/index.html', context)
-
-
-class LTIToolConfigView(View):
-    LAUNCH_URL = LTI_SETUP.get('LAUNCH_URL', 'syllabuspdf:process_lti_launch_request')
-    """
-    Outputs LTI configuration XML for Canvas as specified in the IMS Global Common Cartridge Profile.
-    The XML produced by this view can either be copy-pasted into the Canvas tool
-    settings, or exposed as an endpoint to Canvas by linking to this view.
-    """
-    def get_launch_url(self, request):
-        '''
-        Returns the launch URL for the LTI tool. When a secure request is made,
-        a secure launch URL will be supplied.
-        '''
-        if request.is_secure():
-            host = 'https://' + request.get_host()
-        else:
-            host = 'http://' + request.get_host()
-        url = host + reverse(self.LAUNCH_URL)
-        return url;
-
-    def set_ext_params(self, lti_tool_config):
-        '''
-        Sets extension parameters on the ToolConfig() instance.
-        This includes vendor-specific things like the course_navigation
-        and privacy level.
-        EXAMPLE_EXT_PARAMS = {
-            "canvas.instructure.com": {
-                "privacy_level": "public",
-                "course_navigation": {
-                    "enabled": "true",
-                    "default": "disabled",
-                    "text": "MY tool (localhost)",
-                }
-            }
-        }
-        '''
-        EXT_PARAMS = LTI_SETUP.get("EXTENSION_PARAMETERS", {})
-        for ext_key in EXT_PARAMS:
-            for ext_param in EXT_PARAMS[ext_key]:
-                ext_value = EXT_PARAMS[ext_key][ext_param]
-                lti_tool_config.set_ext_param(ext_key, ext_param, ext_value)
-
-    def get_tool_config(self, request):
-        '''
-        Returns an instance of ToolConfig().
-        '''
-        launch_url = self.get_launch_url(request)
-        return ToolConfig(
-            title=LTI_SETUP['TOOL_TITLE'],
-            description=LTI_SETUP['TOOL_DESCRIPTION'],
-            launch_url=launch_url,
-            secure_launch_url=launch_url,
-        )
-
-    def get(self, request, *args, **kwargs):
-        '''
-        Returns the LTI tool configuration as XML.
-        '''
-        lti_tool_config = self.get_tool_config(request)
-        self.set_ext_params(lti_tool_config)
-        return HttpResponse(lti_tool_config.to_xml(), content_type='text/xml', status=200)
